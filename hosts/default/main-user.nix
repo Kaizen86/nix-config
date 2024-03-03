4e83b15904c39119c94836178f0928a18d300e6a
Kaizen86 <danielbridgewater87@gmail.com>
2024-03-03 16:04:11 +0000

    Begin versioning
    I am staring into the depths of a bottomless NixOS rabbit hole filled with possibilities and I don't know where to start. This is either a horrible mistake or a great idea. We'll find out in a few years!
    In the event my future self is reading this and trying to remember how it all went wrong, it's because the notion of exactly reproducing my system on multiple devices seemed very appealing. Remember trying to match KDE settings on your laptop?

---
+++ hosts/default/main-user.nix
@@ -0,0 +1,28 @@
+{ lib, config, pkgs, ... }:
+
+let
+  cfg = config.main-user;
+in
+{
+  options = {
+    main-user.enable
+      = lib.mkEnableOption "enable user module";
+
+    main-user.userName = lib.mkOption {
+      default = "user";
+      description = ''
+        username
+      '';
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    users.users.${cfg.userName} = {
+      isNormalUser = true;
+      description = "Kaizen";
+      extraGroups = [ "networkmanager" "wheel" ];
+      shell = pkgs.bash;
+    };
+  };
+}
+
