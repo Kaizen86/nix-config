26c17599168a72c9cfbcbccfb78962fbf3864810
Kaizen86 <danielbridgewater87@gmail.com>
2024-03-09 20:15:24 +0000

    Keep track of what host to build

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,20 @@
 #!/run/current-system/sw/bin/bash
-# I keep forgetting this command so I'm putting it in here.
-# In the future this will probably also commit/push whatever I've changed.
-sudo nixos-rebuild switch --flake /home/kaizen/nix-config#tower
+# 
+
+# Find where this script is stored
+CONFIG_ROOT=$(dirname "$0")
+
+# Read what host configuration this machine should use
+if [ -f $CONFIG_ROOT/.host_id ]; then
+  host_id=$(< $CONFIG_ROOT/.host_id)
+fi
+# Check if the variable is empty
+if [ -z $host_id ]; then
+  echo .host_id is missing or empty! It should contain one of the following:
+  ls $CONFIG_ROOT/hosts
+  exit 1
+fi
+
+# Rebuild the system
+sudo nixos-rebuild switch --flake /home/kaizen/nix-config#$host_id
 
