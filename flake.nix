94c1c967eae00296831a8cdbfe9d42215544bc70
Kaizen86 <danielbridgewater87@gmail.com>
2024-03-15 16:17:28 +0000

    Add nix-on-droid host
    "My name is Connor, I'm the Android sent by Cyberlife."

--- flake.nix
+++ flake.nix
@@ -8,9 +8,14 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nix-on-droid = {
+      url = "github:nix-community/nix-on-droid/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs = { self, nixpkgs, nix-on-droid, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
@@ -18,21 +23,30 @@
     {
     
       nixosConfigurations = {
-          tower = nixpkgs.lib.nixosSystem {
-            specialArgs = {inherit inputs;};
-            modules = [ 
-              ./hosts/tower/configuration.nix
-              inputs.home-manager.nixosModules.default
-            ];
-          };
-          laptop = nixpkgs.lib.nixosSystem {
-            specialArgs = {inherit inputs;};
-            modules = [ 
-              ./hosts/laptop/configuration.nix
-              inputs.home-manager.nixosModules.default
-            ];
-          };
+        tower = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [ 
+            ./hosts/tower/configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+        laptop = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [ 
+            ./hosts/laptop/configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
       };
+
+      nixOnDroidConfigurations = {
+        connor = nix-on-droid.lib.nixOnDroidConfiguration {
+           modules = [
+            ./hosts/connor/configuration.nix
+          ];
+        };
+      };
+
     };
 }
 
