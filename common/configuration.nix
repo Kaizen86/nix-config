09089626924ebac03e25677933ce9de84bb51afe
Kaizen86 <danielbridgewater87@gmail.com>
2024-04-14 14:18:59 +0100

    Rearrange some stuff

--- common/configuration.nix
+++ common/configuration.nix
@@ -7,12 +7,17 @@
 {
   imports =
     [
-      # Include the results of the hardware scan.
-      #inputs.home-manager.nixosModules.default
       ./main-user.nix
     ];
 
-
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+  # Enable flakes :)
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Make /tmp volatile
@@ -238,12 +243,4 @@
   environment.sessionVariables = rec { 
     GPG_TTY = "$(tty)";
   };
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
 }
