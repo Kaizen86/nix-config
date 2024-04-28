d4db37795a1405a0f0a2e335d8774c16aa89e432
Kaizen86 <danielbridgewater87@gmail.com>
2024-04-28 13:28:19 +0100

    Add upgrade flag to rebuild script

--- rebuild.sh
+++ rebuild.sh
@@ -15,8 +15,40 @@ if [ -z $host_id ]; then
   exit 1
 fi
 
-# Rebuild the system
-sudo nixos-rebuild switch --flake /home/kaizen/nix-config#$host_id
+# Parse any command-line options
+while [ "$1" ]; do
+  case "$1" in
+    --help)
+      # todo add help text
+      echo "$0"
+      exit
+      ;;
+    -u|--upgrade)
+      nix flake update
+      sudo nix-channel --update
+      #nix-env -u
+      #rebuild_args="$rebuild_args -I $HOME/nix-config"
+      rebuild_args="$rebuild_args --upgrade"
+      shift
+      ;;
+    -*)
+      echo "${FUNCNAME[0]}: unrecognised option '$1'
+Try '${FUNCNAME[0]} --help' for more information"
+      exit 1
+      ;;
+    *)
+      echo "Ignoring argument '$1'"
+      shift
+      ;;
+  esac
+done
+
+# Rebuild the system and pass any additional arguments
+if [ "$rebuild_args" ]; then
+  echo Rebuilding with options: $rebuild_args
+fi
+echo sudo nixos-rebuild switch --flake /home/kaizen/nix-config#$host_id $rebuild_args
+sudo nixos-rebuild switch --flake /home/kaizen/nix-config#$host_id $rebuild_args
 
 # For reasons I do not yet understand, nixos-rebuild needs .nix files to be tracked by Git, otherwise they will seem invisible.
 if git status | grep -q "Untracked files:"; then
