76c1556c0501b8184260101618df07f411272e5e
Kaizen86 <danielbridgewater87@gmail.com>
2025-08-03 14:13:23 +0000

    Improve comments in collect-garbage.sh

--- collect-garbage.sh
+++ collect-garbage.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 # Delete old systems profiles
 
-# This took me far too long to figure out, but the command must be run as root, otherwise it can't touch most links
-# https://www.reddit.com/r/NixOS/comments/unwip2/deleting_old_system_profiles_what_am_i_missing/
-
 # Find where this script is stored
 config_root=$(dirname "$0")
 
@@ -24,11 +21,14 @@ readback() {
 }
 
 if [ "$USER" == "nix-on-droid" ]; then
-  # Usual nixos-rebuild and nix-channel commands won't work on this host
+  # On Android, 'nix-store --optimise' won't work as creating hard links is forbidden
   echo Using nix-on-droid specific command
   readback nix-collect-garbage -d 
   exit $?
 fi
+
+# This took me far too long to figure out, but the command must be run as root, otherwise it can't touch most links
+# https://www.reddit.com/r/NixOS/comments/unwip2/deleting_old_system_profiles_what_am_i_missing/
 
 readback sudo nix-collect-garbage -d 
 readback sudo nix-store --optimise
