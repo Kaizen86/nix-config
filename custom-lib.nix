ae9e177bce090cdebbc25c086acc8e26e977461b
Kaizen86 <danielbridgewater87@gmail.com>
2025-10-27 19:01:47 +0000

    Refactor custom-lib and tidy documentation
    Used let/in blocks to make it much easier to read.
    
    Spelling out each step in the comments was a bit too verbose.
    Added function documentation

--- custom-lib.nix
+++ custom-lib.nix
@@ -1,39 +1,58 @@
 { lib }:
 
+# TODO: Use asserts to validate function inputs
+
 rec {
+  # Straightforward readDir, gives you a plain list of paths
+  readDir = (path:
+    let
+      dirListing = builtins.readDir path;
+	  itemNames = builtins.attrNames dirListing;
+    in
+      # Convert names to full path
+      map
+        (n: lib.path.append path n)
+		itemNames
+  );
+
+  # Filter names from an attribute set where the predicate (n: v: ...) returns true.
   filterAttrNames = (pred: set:
-    # 3. Get the file names as a list: [ "bar" ]
-    (builtins.attrNames
-      # 2. Select by desired type: { "bar" = "regular"; }
-      (lib.filterAttrs pred set)
-    )
+    let filteredAttrs = lib.filterAttrs pred set;
+    in builtins.attrNames filteredAttrs
   );
 
+  # Get a list of paths in a directory, filtered by item type (e.g. "regular", "directory", etc).
   readDirByType = (type: path:
-    map
-    # 4. Convert names to full path: [ /a/b/c/bar ];
-    (n: lib.path.append path n)
-    (filterAttrNames
-      (n: v: v==type)
-      # 1. Get files/directories in some path: { "foo" = "directory"; "bar" = "regular"; }
-      (builtins.readDir path)
-    )
+    let
+      dirListing = builtins.readDir path;
+      # Select names where the value matches $type
+      matchedItemNames = filterAttrNames
+        (n: v: v==type)
+        dirListing;
+    in
+      # Convert names to full path
+      map
+        (n: lib.path.append path n)
+        matchedItemNames
   );
 
+  # Like readDirByType, but accepts a list of names to filter from the output.
   readDirByTypeExcluding = (type: path: excluding:
-    map
-    # 5. Convert names to full path: [ /a/b/c/bar ];
-    (n: lib.path.append path n)
-    # 4. Filter any excluded names: [ "bar" ]
-    (builtins.filter
-      (n: builtins.any (i: n!=i) excluding)
-      # 3. Get the file names as a list: [ "bar" "baz" ]
-      (filterAttrNames
-        (n: v: v == type)
-        # 1. Get files/directories in some path: { "foo" = "directory"; "bar" = "regular"; "baz" = "regular"; }
-        (builtins.readDir path)
-      )
-    )
+    let
+      dirListing = builtins.readDir path;
+      # Select names where the value matches $type
+      matchedItemNames = filterAttrNames
+        (n: v: v==type)
+        dirListing;
+      # Filter any excluded names
+      filteredItemNames = builtins.filter
+        (n: builtins.any (i: n!=i) excluding)
+        matchedItemNames;
+    in
+      map
+        # Convert names to full path
+        (n: lib.path.append path n)
+        filteredItemNames
   );
 
   # Shortcuts for files/directories
