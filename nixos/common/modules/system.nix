ad7a817fef3698dd5fa1ede598726c90467817e8
Kaizen86 <danielbridgewater87@gmail.com>
2025-11-24 12:29:36 +0000

    Tidy up system locale settings

--- nixos/common/modules/system.nix
+++ nixos/common/modules/system.nix
@@ -1,6 +1,8 @@
 { ... }:
 
-{
+let
+  locale = "en_GB.UTF-8";
+in {
   # Make /tmp volatile
   boot.tmp.useTmpfs = true;
 
@@ -11,22 +13,22 @@
 
   # Set your time zone.
   time.timeZone = "Europe/London";
- 
+
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
- 
+  i18n.defaultLocale = locale;
+
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
+    LC_ADDRESS = locale;
+    LC_IDENTIFICATION = locale;
+    LC_MEASUREMENT = locale;
+    LC_MONETARY = locale;
+    LC_NAME = locale;
+    LC_NUMERIC = locale;
+    LC_PAPER = locale;
+    LC_TELEPHONE = locale;
+    LC_TIME = locale;
   };
- 
+
   # Configure console keymap
   console.keyMap = "uk";
 
