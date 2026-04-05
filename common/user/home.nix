c4171ce1fd8a7d927b36723258c745d1e8d7b3a6
Kaizen86 <danielbridgewater87@gmail.com>
2026-04-05 03:00:06 +0100

    nix-tryout now detects unfree packages

--- common/user/home.nix
+++ common/user/home.nix
@@ -45,7 +45,14 @@
     # This function immediately starts the program with arguments after it finishes downloading.
     # Optionally allows specifying a different command name by passing --
     (pkgs.writeShellScriptBin "nix-tryout" ''
+        #!/usr/bin/env bash
+        # First argument is always package to use
         package="$1"
+        if [ -z "$package" ]; then
+            echo No package specified
+            exit 1
+        fi
+        # Parse extra arguments
         shift
         case "$1" in
             "--")
@@ -58,8 +65,17 @@
                 ;;
         esac
         args="$@"
-        #echo package=\"$package\" command=\"$command\" args=\"$args\"
-        nix shell nixpkgs#"$package" --command $command $args
+
+        # Check if this package is unfree
+        unfree=$(nix-instantiate --eval --expr "(import <nixpkgs> {}).$package.meta.unfree")
+        # Set magic environment variable if necessary
+        if [ $unfree == true ]; then
+            echo This package is unfree, setting NIXPKGS_ALLOW_UNFREE=1 and proceeding...
+            export NIXPKGS_ALLOW_UNFREE=1
+        fi
+        # Run it
+        nix shell --impure nixpkgs#$package --command $command $args
+        exit $?
     '')
 
     # Pipe grep output to less, with colours enabled
