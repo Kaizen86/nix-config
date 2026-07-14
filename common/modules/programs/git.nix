b0d8cdee69ddb585b3c8447bcb3cbddca42c7a0d
Kaizen86 <danielbridgewater87@gmail.com>
2026-07-14 11:50:54 +0000

    Add colours to git blame based on age
    Adapted from https://gist.github.com/BuonOmo/ce45b51d0cefe949fd0c536a4a60f000

--- common/modules/programs/git.nix
+++ common/modules/programs/git.nix
@@ -19,6 +19,15 @@
         # Credit to @supraerror on the Nix Cult Discord server lmaooo
         bs = ''!git commit -m "$(curl -s --connect-timeout 4 https://www.whatthecommit.com/index.txt || echo 'wip')"'';
       };
+
+      # https://gist.github.com/BuonOmo/ce45b51d0cefe949fd0c536a4a60f000
+      color.blame = {
+        highlightRecent = "237, 20 month ago, 238, 19 month ago, 239, 18 month ago, 240, 17 month ago, 241, 16 month ago, 242, 15 month ago, 243, 14 month ago, 244, 13 month ago, 245, 12 month ago, 246, 11 month ago, 247, 10 month ago, 248, 9 month ago, 249, 8 month ago, 250, 7 month ago, 251, 6 month ago, 252, 5 month ago, 253, 4 month ago, 254, 3 month ago, 231, 2 month ago, 230, 1 month ago, 229, 3 weeks ago, 228, 2 weeks ago, 227, 1 week ago, 226";
+      };
+      blame = {
+        coloring = "highlightRecent";
+        date = "human";
+      };
     };
   };
 }
