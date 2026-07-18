047b0c1b4bc68177b50e23e8aeb7f7d4fc30ed49
Kaizen86 <danielbridgewater87@gmail.com>
2026-07-18 06:31:16 +0000

    Add FIXME note for hm ssh default host config
    The option is deprecated. Possibly related to this?
    https://github.com/nix-community/home-manager/issues/7266#issue-3146333765

--- common/user/home.nix
+++ common/user/home.nix
@@ -27,6 +27,10 @@
     }) (customLib.fs.listFiles ./dotfiles/config))
   );
 
+  # FIXME: evaluation warning: kaizen profile: `programs.ssh` default values will be removed in the future.
+  # Consider setting `programs.ssh.enableDefaultConfig` to false,
+  # and manually set the default values you want to keep at
+  # `programs.ssh.settings."*"`.
   programs.ssh = {
     enable = true;
     settings = {
