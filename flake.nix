bb82bc6c145f7f8e30282b4e8a7bd07152c11a8e
Kaizen86 <danielbridgewater87@gmail.com>
2026-08-22 12:24:44 +0000

    connor: Can now use latest nixpkgs
    https://github.com/nix-community/nix-on-droid/pull/529 got merged, yippee! :D
    
    Pinned to nix-on-droid master branch because this is a recent change,
    but hopefully someday there will be another release branch.
    Note: requires bootstrap at https://nix-on-droid.unboiled.info/bootstrap/

--- flake.nix
+++ flake.nix
@@ -18,17 +18,8 @@
 
     # For 'connor' host
     nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs-droid";
-      # Override to not use 24.05
-      inputs.home-manager.follows = "home-manager-droid";
-    };
-    # Temporary fix until nix-on-droid merges PR #529 (proot-termux update)
-    # See: https://github.com/nix-community/nix-on-droid/issues/495
-    nixpkgs-droid.url = "github:NixOS/nixpkgs/5d874ac46894c896119bce68e758e9e80bdb28f1";
-    home-manager-droid = {
-      url = "github:nix-community/home-manager/4de84265d7ec7634a69ba75028696d74de9a44a7";
-      inputs.nixpkgs.follows = "nixpkgs-droid";
+      url = "github:nix-community/nix-on-droid/master";
+      inputs.home-manager.follows = "home-manager";
     };
   };
 
@@ -57,10 +48,9 @@
 
       nixOnDroidConfigurations = {
         connor = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-           pkgs = import inputs.nixpkgs-droid { system = "aarch64-linux"; };
+           pkgs = import inputs.nixpkgs { system = "aarch64-linux"; };
            extraSpecialArgs = {
              inherit customLib;
-             newPkgs = import nixpkgs { system = "aarch64-linux"; };
            };
            # I have attempted to include ./common for connor host but it just isn't happening. This makes sense; it's not a NixOS system.
            # I did at least manage to get its tweaked version of home-manager working for most user preferences. (the home.nix config import is in non-nixos/connor/configuration.nix)
